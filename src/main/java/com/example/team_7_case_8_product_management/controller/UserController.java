6d3928371104f7fd37008ce86ed1e09d7193917d
misha <wolfhe4rt@yandex.ru>
2024-08-20 16:25:57 +0300

    some commit

--- src/main/java/com/example/team_7_case_8_product_management/controller/UserController.java
+++ src/main/java/com/example/team_7_case_8_product_management/controller/UserController.java
@@ -4,29 +4,35 @@ import com.example.team_7_case_8_product_management.model.TokenModel;
 import com.example.team_7_case_8_product_management.model.User;
 import com.example.team_7_case_8_product_management.service.UserService;
 import lombok.RequiredArgsConstructor;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
 
 import java.time.LocalDate;
-import java.util.List;
 
 @RestController
 @RequiredArgsConstructor
 public class UserController {
     final UserService userService;
 
+    @CrossOrigin
     @PostMapping("/v1/api/auth")
     public void registerUser(@RequestBody User user) {
         userService.registerUser(user);
     }
 
+    @CrossOrigin
     @PostMapping("/v1/api/login")
-    public TokenModel getToken(@RequestBody User user) {
-        return userService.loginUser(user);
+    public ResponseEntity<TokenModel> getToken(@RequestBody User user) {
+        return ResponseEntity.status(HttpStatus.CREATED)
+                .header("Access-Control-Allow-Origin", "*")
+                .header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept, Authorization")
+                .header("Access-Control-Allow-Credentials", "true")
+                .header("Access-Control-Allow-Methods", "GET,POST,PUT,DELETE,OPTIONS,HEAD")
+                .body(userService.loginUser(user));
     }
 
+    @CrossOrigin
     @GetMapping("/v1/api/user")
     public User getUser() {
         return new User(1l, "misha", "misha", "kaskin", "USER", LocalDate.now());
