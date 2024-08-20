c10e954c1179fd4a5362f401b4a349d30ec5e825
misha <wolfhe4rt@yandex.ru>
2024-08-20 16:55:06 +0300

    another-commit

--- src/main/java/com/example/team_7_case_8_product_management/controller/UserController.java
+++ src/main/java/com/example/team_7_case_8_product_management/controller/UserController.java
@@ -4,35 +4,32 @@ import com.example.team_7_case_8_product_management.model.TokenModel;
 import com.example.team_7_case_8_product_management.model.User;
 import com.example.team_7_case_8_product_management.service.UserService;
 import lombok.RequiredArgsConstructor;
-import org.springframework.http.HttpStatus;
-import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 import java.time.LocalDate;
 
+@CrossOrigin
 @RestController
 @RequiredArgsConstructor
 public class UserController {
     final UserService userService;
 
-    @CrossOrigin
     @PostMapping("/v1/api/auth")
     public void registerUser(@RequestBody User user) {
         userService.registerUser(user);
     }
 
-    @CrossOrigin
     @PostMapping("/v1/api/login")
-    public ResponseEntity<TokenModel> getToken(@RequestBody User user) {
-        return ResponseEntity.status(HttpStatus.CREATED)
-                .header("Access-Control-Allow-Origin", "*")
-                .header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept, Authorization")
-                .header("Access-Control-Allow-Credentials", "true")
-                .header("Access-Control-Allow-Methods", "GET,POST,PUT,DELETE,OPTIONS,HEAD")
-                .body(userService.loginUser(user));
+    public TokenModel getToken(@RequestBody User user) {
+        return userService.loginUser(user);
+//        return ResponseEntity.status(HttpStatus.CREATED)
+//                .header("Access-Control-Allow-Origin", "*")
+//                .header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept, Authorization")
+//                .header("Access-Control-Allow-Credentials", "true")
+//                .header("Access-Control-Allow-Methods", "GET,POST,PUT,DELETE,OPTIONS,HEAD")
+//                .body(userService.loginUser(user));
     }
 
-    @CrossOrigin
     @GetMapping("/v1/api/user")
     public User getUser() {
         return new User(1l, "misha", "misha", "kaskin", "USER", LocalDate.now());
